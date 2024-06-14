7d95a8be66c3d847d3fcf571b1064e7a44f18f72
Yuri Claro <yuri.claro@outlook.com>
2024-06-14 11:08:20 -0300

    add: Task repository

---
+++ src/main/java/com/yuriclaro/todolist/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.yuriclaro.todolist.repository;
+
+import com.yuriclaro.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
